08d8f15b96935de9eea776ca7e73888dc6ffbd66
Abdul Kevin Alexis <lildiop2@gmail.com>
2022-04-26 20:50:20 -0300

    first commit

---
+++ src/compilador/Tag.java
@@ -0,0 +1,29 @@
+package compilador;
+
+public class Tag {
+	 public final static int
+	 //Palavras reservadas
+	 PRG = 256,
+	 BEG = 257,
+	 END = 258,
+	 TYPE = 259,
+	 INT = 260,
+	 CHAR = 261,
+	 BOOL = 262,
+//	 …
+	 //Operadores e pontuação
+//	 ...
+	 EQ = 288,
+	 GE = 289,
+	 LE = 290,
+	 NE = 291,
+//	 ...
+	 //Outros tokens
+	 NUM = 278,
+	 ID = 279,
+	 AND = 0,
+	 OR = 0,
+	TRUE = 0,
+	FALSE = 0;
+	public static final int IF = 0;
+	}
